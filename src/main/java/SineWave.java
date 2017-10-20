149dee6c6e84e78a74863e82ef767b67191995a0
naimad90 <naimad90@bitbucket.org>
2017-10-20 21:27:07 +0200

    SineWave convert rad <-> deg

--- src/main/java/SineWave.java
+++ src/main/java/SineWave.java
@@ -1,7 +1,60 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Damian on 2017-10-20.
- */
 public class SineWave {
+
+    private double amplitude;
+    private double frequency;
+    private double phase;
+
+    public SineWave(double amplitude, double frequency, double phase){
+        setAmplitude(amplitude);
+        setFrequency(frequency);
+        setPhaseRad(phase);
+    }
+
+    public SineWave(double amplitude, double frequency, double phase, boolean deg){
+        this(amplitude, frequency, deg ? degToRad(phase) : phase);
+    }
+
+    public SineWave(){
+        this(0.0, 0.0, 0.0);
+    }
+
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public double getPhaseRad() {
+        return phase;
+    }
+
+    public double getPhaseDeg() {
+        return radToDeg(phase);
+    }
+
+    public void setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+    public void setPhaseRad(double phase) {
+        this.phase = phase;
+    }
+
+    public void setPhaseDeg(double phase) {
+        this.phase = degToRad(phase);
+    }
+
+    private static double degToRad(double deg){
+        return (Math.PI/180.0) * deg;
+    }
+
+    private static double radToDeg(double rad){
+        return (180.0/Math.PI) * rad;
+    }
 }
--- src/main/java/Wave.java
+++ src/main/java/Wave.java
@@ -1,7 +1,4 @@
-package PACKAGE_NAME;
 
-/**
- * Created by Damian on 2017-10-20.
- */
 public class Wave {
+
 }
